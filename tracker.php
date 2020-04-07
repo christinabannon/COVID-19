59d9999829917782309e14cfb5991f90d0d69de1
Christina Bannon <bannon82@elvix2.rowan.edu>
2020-04-06 23:05:48 -0400

    making sure everything is up to date

--- tracker.php
+++ tracker.php
@@ -32,7 +32,7 @@
 </div>
 
 <!-- Current totals chart -->
-<div class="container">
+<div class="container" style="vertical-align: middle;">
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['line']});
